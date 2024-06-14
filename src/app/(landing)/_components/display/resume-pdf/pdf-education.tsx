c61b868db5dc5e12025dea58167f14121b9ccf5d
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-14 11:42:50 -0300

    34: create the pdf education to render the user education details in react pdf

---
+++ src/app/(landing)/_components/display/resume-pdf/pdf-education.tsx
@@ -0,0 +1,71 @@
+import {
+    ResumePDFText,
+    ResumePDFSection,
+    ResumePDFBulletList,
+} from './common/resume-pdf-components';
+import { View } from "@react-pdf/renderer";
+import type { Education } from "@/store/resume-types";
+import { customHTMLParser } from '@/lib/custom-html-parser';
+import { styles, spacing } from "./resume-pdf-utils/resume-pdf-styles";
+
+type ResumeEducationProps = {
+    heading: string;
+    themeColor: string;
+    educations: Education[];
+}
+
+export const ResumeEducation = ({
+    heading,
+    educations,
+    themeColor,
+}: ResumeEducationProps) => {
+    return (
+        <ResumePDFSection themeColor={themeColor} heading={heading} >
+            {educations.map(({
+                gpa,
+                school,
+                degree,
+                endDate,
+                startDate,
+                descriptions,
+            }, index) => {
+                const showDescriptions = descriptions !== "";
+                return (
+                    <View key={index}>
+                        <ResumePDFText bold={true}>{school}</ResumePDFText>
+                        <View style={{
+                            ...styles.flexRowBetween,
+                            marginTop: spacing["2"]
+                        }}
+                        >
+                            <ResumePDFText>
+                                {`${gpa ? `${degree} - ${Number(gpa) ? gpa + " GPA" : gpa}` : degree}`}
+                            </ResumePDFText>
+                            <View style={{
+                                ...styles.flexRow,
+                                gap: spacing["2"],
+                                alignItems: "center"
+                            }}
+                            >
+                                <ResumePDFText>
+                                    {startDate}
+                                </ResumePDFText>
+                                <ResumePDFText>
+                                    {endDate ? "|" : ""}
+                                </ResumePDFText>
+                                <ResumePDFText>
+                                    {endDate}
+                                </ResumePDFText>
+                            </View>
+                        </View>
+                        {showDescriptions && (
+                            <View style={{ ...styles.flexCol, marginTop: `-${spacing["4"]}` }}>
+                                <ResumePDFBulletList items={customHTMLParser(descriptions || "")} />
+                            </View>
+                        )}
+                    </View>
+                )
+            })}
+        </ResumePDFSection>
+    )
+}
