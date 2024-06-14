0df052b745deeecda5aeeee1526c5c3acbfaaff6
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-14 09:15:06 -0300

    20: created a constant variables for resume dimensions

---
+++ src/lib/resume-dimensions.ts
@@ -0,0 +1,11 @@
+// RESUME DIMENSIONS
+const A4_HEIGHT_PT = 842;
+const LETTER_HEIGHT_PT = 792;
+export const PX_PER_PT = 4 / 3;
+export const A4_WIDTH_PT = 595;
+export const LETTER_WIDTH_PT = 612;
+export const A4_WIDTH_PX = A4_WIDTH_PT * PX_PER_PT;
+export const A4_HEIGHT_PX = A4_HEIGHT_PT * PX_PER_PT;
+export const LETTER_WIDTH_PX = LETTER_WIDTH_PT * PX_PER_PT;
+export const LETTER_HEIGHT_PX = LETTER_HEIGHT_PT * PX_PER_PT;
+export const DEBUG_RESUME_PDF_FLAG: true | undefined = undefined;
