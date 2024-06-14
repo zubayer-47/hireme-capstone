3c421e59128452ae803e704036000239cb3b7aa9
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-14 07:45:44 -0300

    9: built flexbox spacer component

---
+++ src/components/flexbox-spacer.tsx
@@ -0,0 +1,14 @@
+export const FlexboxSpacer = ({
+    maxWidth,
+    minWidth = 0,
+    className = "",
+}: {
+    maxWidth: number;
+    minWidth?: number;
+    className?: string;
+}) => (
+    <div
+        className={`invisible shrink-[10000] grow ${className}`}
+        style={{ maxWidth: `${maxWidth}px`, minWidth: `${minWidth}px` }}
+    />
+);
