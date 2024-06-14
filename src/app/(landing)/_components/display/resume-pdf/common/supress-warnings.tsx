a52a9faee0bfacaff29188c5a598bbe29d69c1dc
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-14 10:22:19 -0300

    28: create a helper function for supressing the warnings that react-pdf keeps logging

---
+++ src/app/(landing)/_components/display/resume-pdf/common/supress-warnings.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+/**
+ * Suppress ResumePDF development errors.
+ */
+if (typeof window !== "undefined" && window.location.hostname === "localhost") {
+    const consoleError = console.error;
+    const SUPPRESSED_WARNINGS = ["DOCUMENT", "PAGE", "TEXT", "VIEW"];
+    console.error = function filterWarnings(msg, ...args) {
+        if (!SUPPRESSED_WARNINGS.some((entry) => args[0]?.includes(entry))) {
+            consoleError(msg, ...args);
+        }
+    };
+}
+
+export const SuppressWarnings = () => {
+    return <></>;
+};
