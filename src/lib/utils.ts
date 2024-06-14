bba38f0fdff14c310ddc03fe1014270c0193680e
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-14 07:54:57 -0300

    12: added the globals-css variables in utils file under the lib folder

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const CSS_VARIABLES = {
+  "--top-nav-bar-height": "3.5rem",
+  "--resume-control-bar-height": "3rem",
+  "--resume-padding": "1.5rem",
+} as const;
