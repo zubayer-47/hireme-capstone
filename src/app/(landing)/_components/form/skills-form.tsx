b9506fe5f430ffa6c0ad2b7d22d26f445474fd1b
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-14 08:27:17 -0300

    15: built the skills form for the resume form

---
+++ src/app/(landing)/_components/form/skills-form.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Card } from "@/components/card";
+import { useResumeStore } from "@/store/resume-store"
+import { Tiptap } from "@/components/text-editor/tiptap";
+import { DynamicInput } from "@/components/dynamic-input";
+
+export const SkillsForm = () => {
+    const { skills, setSkills } = useResumeStore();
+
+    return (
+        <Card >
+            {skills.map((skill, index) => (
+                <div className="space-y-4 mt-4" key={index}>
+                <hgroup className="grid gap-2">
+                    <DynamicInput
+                        id="skills"
+                        type="text"
+                        labelName="Skill Title"
+                        value={skill.heading}
+                        placeholder="Frontend | Backend | Database"
+                        onChange={(e) => { setSkills(index, "heading", e.target.value) }}
+                    />
+                </hgroup>
+                <hgroup className="grid gap-2">
+                    <label htmlFor="skills" className="font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Featured Skills</label>
+                    <Tiptap content={skill.featuredSkills} onChange={(newText: string) => setSkills(index, "featuredSkills", newText)} />
+                </hgroup>
+                </div>
+            ))}
+        </Card>
+    )
+}
