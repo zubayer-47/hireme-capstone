b99af153def6daade1a1a1aba4077f4d62b502a9
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-15 17:33:02 -0300

    52: created a convex file provider to wrap it in layout file

---
+++ src/providers/convex-client-provider.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export const ConvexClientProvider = ({ children }: { children: React.ReactNode }) => {
+    return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+}
