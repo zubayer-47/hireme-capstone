79409fe9c2a1b19bff2bfb2465177e1a757e1f91
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-15 08:25:20 -0300

    39: built the home page for the landing page

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <main className="mx-autorelative h-full w-full  bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+            <section className="max-w-2xl">
+
+            </section>
+        </main>
+    )
+}
