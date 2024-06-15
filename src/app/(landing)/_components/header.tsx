a3e80dc9780b99595a26fb1c93123c09df96bc53
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-15 09:07:31 -0300

    40: built the header for landing page

--- src/app/(landing)/_components/header.tsx
+++ src/app/(landing)/_components/header.tsx
@@ -1,7 +1,85 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { 
+  Menu, 
+  MenuItem,
+  HoveredLink,  
+  ProductItem 
+} from "@/components/animation/navbar-menu";
+import { Button } from "@/components/ui/button";
+
+
+
 export const Header = () => {
     return (
-        <header>
-            
+        <header className="relative w-full flex items-center justify-center">
+            <Navbar className="top-2" />
         </header>
     )
 }
+
+function Navbar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null);
+    return (
+      <div
+        className={cn("fixed top-10 inset-x-0 max-w-6xl mx-auto z-50", className)}
+      >
+        <Menu setActive={setActive}>
+          <Image src="/svg/logo.svg" alt="Letter H logo" width="25" height="25"  />
+          <Link href="#" className="text-white">About</Link>
+          <Link href="#" className="text-white">Contact</Link>
+          <MenuItem setActive={setActive} active={active} item="Services">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/web-dev">Web Development</HoveredLink>
+              <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+              <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+              <HoveredLink href="/branding">Branding</HoveredLink>
+            </div>
+          </MenuItem>
+          <MenuItem setActive={setActive} active={active} item="Products">
+            <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+              <ProductItem
+                title="Algochurn"
+                href="https://algochurn.com"
+                src="https://assets.aceternity.com/demos/algochurn.webp"
+                description="Prepare for tech interviews like never before."
+              />
+              <ProductItem
+                title="Tailwind Master Kit"
+                href="https://tailwindmasterkit.com"
+                src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
+                description="Production ready Tailwind css components for your next project"
+              />
+              <ProductItem
+                title="Moonbeam"
+                href="https://gomoonbeam.com"
+                src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
+                description="Never write from scratch again. Go from idea to blog in minutes."
+              />
+              <ProductItem
+                title="Rogue"
+                href="https://userogue.com"
+                src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
+                description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
+              />
+            </div>
+          </MenuItem>
+          <MenuItem setActive={setActive} active={active} item="Pricing">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/hobby">Hobby</HoveredLink>
+              <HoveredLink href="/individual">Individual</HoveredLink>
+              <HoveredLink href="/team">Team</HoveredLink>
+              <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+            </div>
+          </MenuItem>
+          <Button>
+            Sign In
+          </Button>
+        </Menu>
+      </div>
+    );
+  }
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,8 +1,10 @@
+import { Header } from "./_components/header";
+
 export default function Home() {
     return (
-        <main className="mx-autorelative h-full w-full  bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-            <section className="max-w-2xl">
-
+        <main className="mx-auto relative h-full w-full  bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+            <section className="max-w-6xl">
+                <Header />
             </section>
         </main>
     )
