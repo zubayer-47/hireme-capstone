8bbe4ecd511b1d3ab5b310afd59bc993995e6035
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-15 17:37:17 -0300

    53: integrate clerk with convex using jwt

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://prompt-shiner-30.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
--- src/providers/convex-client-provider.tsx
+++ src/providers/convex-client-provider.tsx
@@ -1,8 +1,18 @@
 "use client";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+import { ConvexReactClient } from "convex/react"
+import { ClerkProvider, useAuth } from "@clerk/nextjs"
+import { ConvexProviderWithClerk } from "convex/react-clerk"
+
+
+const client = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 export const ConvexClientProvider = ({ children }: { children: React.ReactNode }) => {
-    return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+    return (
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+            <ConvexProviderWithClerk client={client} useAuth={useAuth}>
+                {children}
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    )
 }
