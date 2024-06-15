ca419fc93d766687232db37ad9418df2aeb86938
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-15 18:25:52 -0300

    57: created a custom hook for checking user if they currently login or loading the login user

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,5 @@
 import { ConvexError } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, MutationCtx, QueryCtx, query  } from "./_generated/server";
 
 export const store = mutation({
     args: {},
@@ -8,11 +8,14 @@ export const store = mutation({
 
         if (!identity) throw new ConvexError("Unauthorized!");
 
+        // Check if the user is already stored in the database
         const user = await ctx.db
             .query("users")
             .withIndex("by_token", (q) => q.eq("tokenIdentifier", identity.tokenIdentifier)).unique();
 
         if (user !== null) {
+            // If the user is already in the database but the name change, 
+            // Patch the new name
             if (user.name !== identity.name) {
                 await ctx.db.patch(user._id, { name: identity.name})
             }
@@ -20,9 +23,36 @@ export const store = mutation({
             return user._id;
         }
 
+        // If it's a new user, we create a new one
         return await ctx.db.insert("users", {
             name: identity.name!,
             tokenIdentifier: identity.tokenIdentifier,
         })
+    }
+})
+
+export const getUser = async (
+    ctx: QueryCtx | MutationCtx,
+    tokenIdentifier: string
+) => {
+    const user = await ctx.db.query("users").withIndex("by_token", (q) => q.eq("tokenIdentifier", tokenIdentifier)).first();
+
+    if (!user) throw new ConvexError("User not found!");
+
+    return user;
+}
+
+export const getSelf = query({
+    args: {},
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) return null;
+
+        const user = await getUser(ctx, identity.tokenIdentifier);
+
+        if (!user) return null;
+
+        return user;
     }
 })
---
+++ src/hooks/use-store-user-effect.ts
@@ -0,0 +1,37 @@
+"use client";
+
+
+import { useUser } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+import { api } from "../../convex/_generated/api";
+import { Id } from "../../convex/_generated/dataModel";
+import { useConvexAuth, useMutation } from "convex/react"
+
+export const useStoreUserEffect = () => {
+    const { isLoading, isAuthenticated } = useConvexAuth();
+    const { user } = useUser();
+
+    const [userId, setUserId] = useState<Id<"users"> | null>(null);
+    const storeUser = useMutation(api.users.store);
+
+    useEffect(() => {
+        // if the user is not login don't do anything
+        if (!isAuthenticated) return;
+
+        // Store the new user in the database
+        const createUser = async () => {
+            const id = await storeUser();
+            setUserId(id)
+        }
+
+        createUser();
+        return () => setUserId(null)
+
+    }, [isAuthenticated, storeUser, user?.id])
+
+    return {
+        isLoading: isLoading || (isAuthenticated && userId === null),
+        isAuthenticated: isAuthenticated && userId !== null
+    }
+    
+}
