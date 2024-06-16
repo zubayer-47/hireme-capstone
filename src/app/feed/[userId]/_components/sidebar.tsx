081f79584af5cfc521fb189582f060f5c3b8a1e6
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-16 07:01:49 -0300

    72: added the user profile image and their name then added few links for the sidebar

--- src/app/feed/[userId]/_components/sidebar.tsx
+++ src/app/feed/[userId]/_components/sidebar.tsx
@@ -3,6 +3,8 @@ import {
     Sheet,
     Hammer,
     ArrowUp,
+    Bookmark,
+    CalendarClock,
     MessageSquareMore,
 } from "lucide-react";
 import Link from "next/link";
@@ -11,12 +13,15 @@ import {
     CardHeader,
     CardContent,
     CardDescription,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
+import Image from "next/image";
 import { AppLogo } from "@/components/app-logo";
 import { Button } from "@/components/ui/button";
+import { currentUser } from "@clerk/nextjs/server";
 
 
-export const Sidebar = ({ userId }: { userId: string }) => {
+export const Sidebar = async ({ userId }: { userId: string }) => {
+    const user = await currentUser();
     return (
         <div className="flex h-full max-h-screen flex-col gap-2">
             <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
@@ -27,7 +32,12 @@ export const Sidebar = ({ userId }: { userId: string }) => {
                 </Button>
             </div>
             <div className="flex-1">
-                <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                <nav className="grid items-start px-2 text-sm font-medium lg:px-4 space-y-4">
+                    <hgroup className="flex items-center gap-3 bg-neutral-500 rounded-md py-1 px-1">
+                        <Image src={user?.imageUrl!} alt="User Profile Picture" width={20} height={20} className="object-cover rounded-md" />
+                        <h2 className="capitalize text-neutral-100">{user?.firstName} Feed</h2>
+                    </hgroup>
+                    
                     <ul>
                         <p>Discover</p>
                         <li>
@@ -35,8 +45,17 @@ export const Sidebar = ({ userId }: { userId: string }) => {
                                 href="#"
                                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                             >
+                                <CalendarClock className="h-4 w-4" />
+                                Recent
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="#"
+                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                            >
                                 <MessageSquareMore className="h-4 w-4" />
-                                Discussions
+                                Most Discussed
                             </Link>
                         </li>
                         <li>
@@ -45,7 +64,16 @@ export const Sidebar = ({ userId }: { userId: string }) => {
                                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
                             >
                                 <ArrowUp className="h-4 w-4" />
-                                Most Upvoted
+                                Most Upvotes
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="#"
+                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                            >
+                                <Bookmark className="h-4 w-4" />
+                                Bookmarked
                             </Link>
                         </li>
                     </ul>
