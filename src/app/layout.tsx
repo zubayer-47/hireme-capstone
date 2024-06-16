630842cfe5f2d8c1a3b04c3486359def5c373b65
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-16 07:30:28 -0300

    75: wrap the theme provider in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { ConvexClientProvider } from "@/providers/convex-client-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ConvexClientProvider>
-      <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </ConvexClientProvider>
     </html>
   );
