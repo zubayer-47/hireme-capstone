2a6a5239a30d7c0f22fe3505123a91ce25a0775a
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-21 19:10:37 -0300

    110: create job tracker main page

---
+++ src/app/tracker/page.tsx
@@ -0,0 +1,11 @@
+import { Browser } from "@/components/browser/browser"
+
+export default function JobTrackerPage() {
+    return (
+        <Browser>
+            <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-8 w-full">
+
+            </main>
+        </Browser>
+    )
+}
