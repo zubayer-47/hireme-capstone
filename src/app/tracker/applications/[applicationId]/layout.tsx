4bc41fbe5c53383e7214c358c6cd9f57f133a8ad
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-23 11:45:24 -0300

    167: temporary on fix on getting 404 error on /tracker/applications/:applicationId route

---
+++ src/app/tracker/applications/[applicationId]/layout.tsx
@@ -0,0 +1,11 @@
+import { LoadingSpinner } from "@/components/loading-spinner";
+import { ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+
+export default function EditJobApplicationLayout({ children }: {children: React.ReactNode }) {
+    return (
+        <>  
+            <ClerkLoading><LoadingSpinner /> </ ClerkLoading >
+            <ClerkLoaded>{children}</ClerkLoaded>
+        </>
+    )
+} 
