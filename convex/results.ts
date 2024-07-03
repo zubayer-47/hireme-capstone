aca15957cfdc614e1123fb780ee8997c27ecdd24
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-03 18:13:10 -0300

    189: create a function for saving the ai results on the db

--- convex/results.ts
+++ convex/results.ts
@@ -37,4 +37,43 @@ export const getResults = query({
 
         return results;
     }
+});
+
+export const saveResults = internalMutation({
+    args: {
+        resumeId: v.id("resume"),
+        score: v.optional(v.string()),
+        jobDescriptionSummary: v.object({
+            jobPosition: v.string(),
+            companyName: v.string(),
+            location: v.string(),
+            employmentType: v.union(v.literal("Full-Time"), v.literal("Part-Time"), v.literal("Hybrid")),
+            responsibilities: v.array(v.string()),
+            requiredSkills: v.array(v.string()),
+            preferredQualification: v.array(v.string()),
+            experienceLevel: v.string(),
+            educationalRequirements: v.string(),
+            salaryRange: v.string(),
+            benefits: v.array(v.string())
+        }),
+        extractedKeywords: v.object ({
+            highImportance: v.array(v.string()),
+            mediumImportance: v.array(v.string()),
+            lowImportance: v.array(v.string()),
+        }),
+        userFeedback: v.object({
+            missingKeywords: v.array(v.string()),
+            suggestions: v.array(v.string())
+        })
+    },
+    handler: async (ctx, args) => {
+        const identity = await userIdentity(ctx);
+
+        if (!identity) throw new ConvexError("Unauthorized!");
+
+        await ctx.db.insert("results", {
+            userId: identity._id,
+            ...args
+        })
+    }
 })
