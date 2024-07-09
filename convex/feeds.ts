92039a5df7ee25e7056d97a9ae3c5f45a0f5fb75
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-09 15:21:02 -0300

    230: create function to upvote or upvote a feed

--- convex/feeds.ts
+++ convex/feeds.ts
@@ -12,7 +12,7 @@ async function userIdentity(
         .query("users")
         .withIndex("by_token", (q) => q.eq("tokenIdentifier", identity.tokenIdentifier))
         .unique();
-    
+
     if (!user) return null;
 
     return user;
@@ -29,7 +29,7 @@ export const generateUploadUrl = mutation(
 )
 
 export const getFeeds = query({
-    args: {}, 
+    args: {},
     handler: async (ctx, args) => {
         const identity = await userIdentity(ctx);
 
@@ -44,18 +44,74 @@ export const createFeed = mutation({
         bio: v.string(),
         fileId: v.id("_storage"),
         fileUrl: v.string(),
-        upvotes: v.number(),
-        downVotes: v.number(),
+        upvoteCount: v.number(),
+        downvoteCount: v.number(),
         username: v.string(),
         tags: v.array(v.string()),
-    }, 
+    },
     handler: async (ctx, args) => {
         const identity = await userIdentity(ctx);
 
         if (!identity) throw new ConvexError("Unauthorized!");
 
-        return await ctx.db.insert("feeds", { ...args, userId: identity._id} )
+        return await ctx.db.insert("feeds", { ...args, userId: identity._id })
     }
 });
+
+export const editBioTagFeed = mutation({
+    args: {
+        feedId: v.id("feeds"),
+        bio: v.string(),
+        tags: v.array(v.string()),
+    },
+    handler: async (ctx, { bio, tags, feedId }) => {
+        const identity = await userIdentity(ctx);
+
+        if (!identity) throw new ConvexError("Unauthorized!");
+
+        const feed = await ctx.db.get(feedId);
+
+        if (!feed) throw new ConvexError("The feed you requested doesn't exist.");
+
+        const updatedFeed = await ctx.db.patch(feedId, {
+            bio,
+            tags,
+        });
+
+        return updatedFeed;
+    }
+});
+
+export const vote = mutation({
+    args: {
+        upvote: v.boolean(),
+        upvoteCount: v.number(),
+        downvoteCount: v.number(),
+        feedId: v.id("feeds")
+    },
+    handler: async (ctx, {
+        upvote,
+        downvoteCount,
+        upvoteCount,
+        feedId }) => {
+        const identity = await userIdentity(ctx);
+
+        if (!identity) throw new ConvexError("Unauthorized!");
+
+        const feed = await ctx.db.get(feedId);
+
+        if (!feed) throw new ConvexError("The feed you requested doesn't exist.");
+
+        if (upvote) {
+            await ctx.db.patch(feedId, {
+                upvoteCount
+            });
+        } else {
+            await ctx.db.patch(feedId, {
+                downvoteCount
+            });
+        }
+    }
+})
 
 
