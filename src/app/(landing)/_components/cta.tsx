ec6dcfced54ccdb00e1bc65fd2820ade7b568c1e
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-11 20:12:02 -0300

    260: fix the styling of the landing section and make it responsive

--- src/app/(landing)/_components/cta.tsx
+++ src/app/(landing)/_components/cta.tsx
@@ -9,15 +9,18 @@ export const CTA = ({ isAuthenticated, userId }: { isAuthenticated: boolean; use
             <article className="px-8 py-12 mx-auto md:px-12 lg:px-32 max-w-7xl">
                 <aside className="p-2 border border-white/[0.2] bg-neutral-800 rounded-3xl">
                     <hgroup className="p-10 text-center bg-neutral-950  border border-white/[0.2] shadow-lg shadow-neutral-500 md:p-20 rounded-3xl">
-                        <p className="text-4xl font-semibold tracking-tighter text-neutral-100">
+                        <p className="text-2xl sm:text-4xl font-semibold tracking-tighter text-neutral-200">
                             Land Your Dream Job Faster
                         </p>
-                        <p className="mt-4 text-base text-gray-500">
+                        <p className="mt-4 text-sm sm:text-base text-gray-400">
                             Build winning applications, track progress effortlessly, and join a supportive community.
                         </p>
-                        <div className="flex flex-col items-center justify-center gap-2 mx-auto mt-8 md:flex-row">
-                            <Link href={path} className="h-11 px-8 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input hover:text-accent-foreground translate-hover bg-app-color hover:bg-app-color/80 translate-hover">
-                                Start Building
+                        <div className="flex flex-col items-center justify-center gap-2 mx-auto mt-6 sm:mt-8 md:flex-row">
+                            <Link href={path} className="p-0.5 relative">
+                                <div className="absolute inset-0 bg-gradient-to-r from-blue-600 to-app-color rounded-lg" />
+                                <div className="px-8 py-2 text-sm bg-neutral-900 rounded-[6px] relative group transition duration-200 text-neutral-200 hover:bg-transparent">
+                                    Start Building
+                                </div>
                             </Link>
                         </div>
                     </hgroup>
