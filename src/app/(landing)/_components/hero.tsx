48c0edcb9d22925ac7ba2415a897e5e673fdce7a
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-11 19:35:43 -0300

    258: fix the styling of the landing features section

--- src/app/(landing)/_components/hero.tsx
+++ src/app/(landing)/_components/hero.tsx
@@ -27,8 +27,8 @@ export const Hero = ({ isAuthenticated, userId }: { isAuthenticated: boolean; us
             <article className="px-8 py-24 mx-auto md:px-12 lg:px-32 max-w-7xl">
                 <aside className="text-center">
                     <h1 className="flex flex-col gap-2 text-xl sm:text-2xl md:text-4xl font-semibold tracking-tighter text-neutral-200 lg:text-5xl text-balance">
-                        <AnimatedTextUnderline /> is designed to simplify and enhance
-                        the job application process for the job seekers.
+                        <AnimatedTextUnderline /> designed to simplify and enhance
+                        job application process for job seekers.
                     </h1>
                     <p className="w-3/4 mx-auto mt-4 text-sm sm:text-base font-medium text-gray-500 text-balance">
                         Your tool to improve your resume and cover letter, gives helpful feedback, and helps you track your job applications.
@@ -47,6 +47,7 @@ export const Hero = ({ isAuthenticated, userId }: { isAuthenticated: boolean; us
                         className="relative h-full p-2 mt-16 sm:mt-24 overflow-hidden bg-neutral-800 shadow- border border-white/[0.2] rounded-3xl">
                         <Image
                             src=""
+                            priority
                             width="1000"
                             height="500"
                             alt="placeholder"
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
     return (
         <main className="w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
             <section className="max-w-6xl mx-auto">
-                <Header isLoading={isLoading} isAuthenticated={isAuthenticated} />
+                <Header isAuthenticated={isAuthenticated} />
                 <Hero isAuthenticated={isAuthenticated} userId={user?._id!}/>
                 <Features />
                 <BentoGridFeatures />
