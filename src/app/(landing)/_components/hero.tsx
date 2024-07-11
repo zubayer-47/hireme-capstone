39b7aee15c602b7f890286f8bc422f64e89923cd
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-11 19:21:38 -0300

    257: style the landing hero component

--- src/app/(landing)/_components/hero.tsx
+++ src/app/(landing)/_components/hero.tsx
@@ -1,30 +1,58 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { useRef } from "react";
+import { 
+    motion, 
+    useScroll, 
+    useTransform } from "framer-motion";
 import { Id } from "@/convex/_generated/dataModel";
-
+import { AnimatedTextUnderline } from "@/components/animation/animated-text-underline";
 
 export const Hero = ({ isAuthenticated, userId }: { isAuthenticated: boolean; userId: Id<"users"> }) => {
     const path = isAuthenticated ? `/feeds/${userId}` : "/auth/sign-in";
+
+    const ref = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["0 1", "1.33 1"]
+    })
+
+    const scale = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+    const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
 
     return (
         <section className="lg:pt-24 md:pt-16 pt-8">
             <article className="px-8 py-24 mx-auto md:px-12 lg:px-32 max-w-7xl">
                 <aside className="text-center">
-                    <h1 className="text-4xl font-semibold tracking-tighter text-neutral-100 lg:text-5xl text-balance">
-                        <span className="text-app-color">HireMe</span> is designed to simplify and enhance
-                        <span className="text-gray-600"> the job application process for the job seekers.</span>
+                    <h1 className="flex flex-col gap-2 text-xl sm:text-2xl md:text-4xl font-semibold tracking-tighter text-neutral-200 lg:text-5xl text-balance">
+                        <AnimatedTextUnderline /> is designed to simplify and enhance
+                        the job application process for the job seekers.
                     </h1>
-                    <p className="w-3/4 mx-auto mt-4 text-base font-medium text-gray-500 text-balance">
+                    <p className="w-3/4 mx-auto mt-4 text-sm sm:text-base font-medium text-gray-500 text-balance">
                         Your tool to improve your resume and cover letter, gives helpful feedback, and helps you track your job applications.
                     </p>
-                    <div className="flex flex-col items-center justify-center gap-2 mx-auto mt-8 md:flex-row">
-                        <Link href={path} className="h-11 px-8 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input hover:text-accent-foreground translate-hover bg-app-color hover:bg-app-color/80 translate-hover">
-                        Start Building
+                    <div className="flex flex-col items-center justify-center gap-2 mx-auto mt-4 sm:mt-8 md:flex-row">
+                        <Link href={path} className="p-0.5 relative">
+                            <div className="absolute inset-0 bg-gradient-to-r from-blue-600 to-app-color rounded-lg" />
+                            <div className="px-8 py-2 text-sm bg-neutral-900 rounded-[6px] relative group transition duration-200 text-neutral-200 hover:bg-transparent">
+                                Start Building
+                            </div>
                         </Link>
                     </div>
-                    <div className="relative h-full p-2 mt-24 overflow-hidden bg-neutral-800 shadow- border border-white/[0.2] rounded-3xl">
-                        <Image src="" alt="placeholder" className="object-cover h-full border border-white/[0.2] bg-neutral-900 shadow-xl shadow-neutral-500 rounded-2xl" width="1000" height="500" />
-                    </div>
+                    <motion.div 
+                        ref={ref}
+                        style={{ scale, opacity }}
+                        className="relative h-full p-2 mt-16 sm:mt-24 overflow-hidden bg-neutral-800 shadow- border border-white/[0.2] rounded-3xl">
+                        <Image
+                            src=""
+                            width="1000"
+                            height="500"
+                            alt="placeholder"
+                            className="object-cover h-full border border-white/[0.2] bg-neutral-900 shadow-xl shadow-neutral-500 rounded-2xl"
+                        />
+                    </motion.div>
                 </aside>
             </article>
         </section>
