ef3f3c95a63587d8f470094849c7ed3f6d6c78c8
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-13 21:17:23 -0300

    292: create the cover letter types for building the store

---
+++ src/store/cover-letter-types.ts
@@ -0,0 +1,80 @@
+// -------------------------------------------------------------
+// ------------------- Resume Store Types ----------------------
+// -------------------------------------------------------------
+export type CoverLetterHeading = {
+    name: string;
+    role: string;
+    email: string;
+    phone: string;
+    date: string;
+    linkedInUrl: string;
+    githubUrl?: string;
+}
+
+export type RecruiterInfo = {
+    name: string;
+    title: string;
+    companyName: string;
+    address: string;
+    cityStateZip: string;
+}
+
+export type Greeting = {
+    greeting: string;
+}
+
+export type FirstParagraph= {
+    text: string;
+}
+
+export type MiddleParagraph = {
+    text: string;
+}
+
+export type ClosingParagraph = {
+    text: string;
+}
+
+export type CoverLetter = {
+    coverLetterHeading: CoverLetterHeading;
+    recruiterInfo: RecruiterInfo;
+    greeting: Greeting;
+    firstParagraph: FirstParagraph;
+    middleParagraph: MiddleParagraph;
+    closingParagraph: ClosingParagraph;
+}
+
+// -------------------------------------------------------------
+// ------------------- Settings Store Types --------------------
+// -------------------------------------------------------------
+export type Settings = {
+    themeColor: string;
+    fontFamily: string;
+    fontSize: string;
+    documentSize: string;
+
+    formOrder: {
+        projects: boolean;
+        skills: boolean;
+        experience: boolean;
+        education: boolean;
+    };
+
+    formHeading: {
+        projects: string;
+        skills: string;
+        experience: string;
+        education: string;
+    };
+
+    formsOrder: DisplayOrderForm[]
+
+}
+
+export type DisplayOrderForm = keyof Settings["formOrder"];
+
+
+
+
+
+
