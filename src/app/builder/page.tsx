e89f71dae41c4b36b3bfb1df7dc0f7df7c24e859
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-18 08:36:07 -0300

    237: fix the builder layout page for an extra sidebar

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -3,10 +3,8 @@ import { DocumentTab } from "./_components/browser/document-tab"
 
 export default function BuilderPage() {
     return (
-        <Browser>
-            <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-8 w-full pt-24">
-               <DocumentTab /> 
-            </main>
-        </Browser>
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-8 w-full pt-24">
+            <DocumentTab /> 
+        </main>
     )
 }
